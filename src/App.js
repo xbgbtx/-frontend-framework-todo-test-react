f28c0e6dd7639b48086d46fbeafc668eda80ef40
xbgbtx <78358155+xbgbtx@users.noreply.github.com>
2021-09-08 11:51:39 +0100

    Functional item input form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import './App.css';
 import React from "react";
+import {useState} from "react";
 
 function App() {
     
     let todo_items = [];
     let done_items = [ "foo", "bar", "baz" ];
 
+    const [ todo_input_text, set_todo_input_text ] = useState("");
+
+    const todo_submit_handler = e =>
+    {
+        e.preventDefault ();
+
+        console.log ( `Submit: ${todo_input_text}` );
+    };
+
+    const todo_change_handler = e =>
+    {
+        todo_input_text += e.target.value;
+        console.log ( e.target.value );
+    };
+
     return (
         <div className="App">
         <ListDisplay list_name="ToDo" list_items={todo_items} />
         <ListDisplay list_name="Done" list_items={done_items} />
-            <TodoItemForm />
+        <TodoItemForm 
+            todo_text={todo_input_text}
+            change_cb={e => set_todo_input_text ( e.target.value )}
+            submit_cb={todo_submit_handler}
+        />
         </div>
     );
 }
@@ -44,50 +64,23 @@ const ListDisplay = ({ list_name, list_items }) =>
 
 }
 
-class TodoItemForm extends React.Component
+const TodoItemForm = ({ todo_text, change_cb, submit_cb }) =>
 {
-    constructor (props)
-    {
-        super (props);
-
-        this.state = { 
-            value : "" 
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-
-    }
-
-    handleChange ( e )
-    {
-        this.setState ({ value : e.target.value });
-    }
-
-    handleSubmit ( e )
-    {
-        e.preventDefault ();
-        alert ( `Submit: ${this.state.value}` );
-    };
-
-    render ()
-    {
-        return (
-            <div>
-                <h1>Add ToDo Item:</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Add ToDo Item:
-                        <input type="text" name="todo-item"
-                            value={this.state.value}
-                            onChange={this.handleChange}
-                        />
-                    </label>
-                    <input type="submit" value="Add Item" />
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div>
+            <h1>Add ToDo Item:</h1>
+            <form onSubmit={submit_cb}>
+                <label>
+                    Add ToDo Item:
+                    <input type="text" name="todo-item"
+                        value={todo_text}
+                        onChange={change_cb}
+                    />
+                </label>
+                <input type="submit" value="Add Item" />
+            </form>
+        </div>
+    );
 }
 
 export default App;
