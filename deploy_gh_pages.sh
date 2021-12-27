dff69b2f5c1df6e72d6b7f3ec37a2ff955ec7e73
Your Name <you@example.com>
2021-12-27 21:35:55 +0000

    Publishing to gh-pages (publish.sh)

--- deploy_gh_pages.sh
+++ deploy_gh_pages.sh
@@ -19,7 +19,7 @@ git worktree prune
 rm -rf ".git/worktrees/$BUILD_DIR/"
 
 echo "Checking out gh-pages branch into \"$BUILD_DIR\""
-git worktree add -B $GH_PAGES_BRANCH $BUILD_DIR
+git worktree add -B $GH_PAGES_BRANCH $BUILD_DIR origin/$GH_PAGES_BRANCH
 
 echo "Removing existing files"
 rm -rf $BUILD_DIR/*
