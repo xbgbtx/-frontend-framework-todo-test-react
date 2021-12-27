31546d0a9f6ac2466ee63943443511bcfd7598c9
Your Name <you@example.com>
2021-12-27 21:10:42 +0000

    WIP gh pages deploy

---
+++ deploy_gh_pages.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+BUILD_CMD="npm run build"
+BUILD_DIR="$PWD/build"
+
+if [ ! -d "$BUILD_DIR" ]; then
+   mkdir "$BUILD_DIR"
+fi
+
+echo "Building to $BUILD_DIR"
+
+if [[ $(git status -s) ]]; then
+   echo "The working directory is dirty. Please commit any pending changes."
+   exit 1;
+fi
+
+#echo "Deleting old publication"
+#rm -rf "$BUILD_DIR"
+#mkdir "$BUILD_DIR"
+#git worktree prune
+#rm -rf .git/worktrees/"$BUILD_DIR"/
+#
+#echo "Checking out gh-pages branch into \"$BUILD_DIR\""
+#git worktree add -B gh-pages "$BUILD_DIR" origin/gh-pages
+#
+#echo "Removing existing files"
+#rm -rf "$BUILD_DIR"/*
+#
+#echo "Copying project submodules"
+#rsync -av --exclude='*.git*' project_submodules/ "$BUILD_DIR"
+#
+#echo "Running Build"
+#$BUILD_CMD
+#
+#echo "Updating gh-pages branch"
+#cd "$BUILD_DIR" && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+#
+#git push origin gh-pages
+#
